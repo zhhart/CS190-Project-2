74f69bab4ebba2b2d47ac330e8ea3e0f3574937c
Steven Kuo <sykuo@uci.edu>
2017-05-02 15:30:37 -0700

    Added Sound Effect for Eating a Pickup Object
    * OnTriggerEnter play the HumanEating Sound
    * Also generated the sound event for humaneating, will need to shorten/crop the eating sound though

--- CS190 Project 2/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ CS190 Project 2/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,6 +14,7 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID BIRDS = 352130103U;
+        static const AkUniqueID PICKUP = 3978245845U;
     } // namespace EVENTS
 
     namespace BANKS
