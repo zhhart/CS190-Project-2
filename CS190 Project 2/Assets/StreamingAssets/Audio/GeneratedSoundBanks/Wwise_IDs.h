fea03a4970ce907c9ec9a8a360c2c15360b9ba1c
Steven Kuo <sykuo@uci.edu>
2017-05-02 23:30:34 -0700

    Adding Sound FX from Wwise (Generating Sounds to Main SoundBank)
    * Added to Wwise Main SoundBank
    +PuttingOnClothes
    +HumanSnoring
    +HumanYawning
    +GettingOutOfBed
    +Brushing

--- CS190 Project 2/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ CS190 Project 2/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,7 +14,12 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID BIRDS = 352130103U;
+        static const AkUniqueID BRUSHING = 3578365137U;
+        static const AkUniqueID GETTINGOUTOFBED = 3250402423U;
+        static const AkUniqueID HUMANSNORING = 2213787648U;
         static const AkUniqueID PICKUP = 3978245845U;
+        static const AkUniqueID PUTTINGONCLOTHES = 1544142421U;
+        static const AkUniqueID YAWNING = 486364660U;
     } // namespace EVENTS
 
     namespace BANKS
@@ -23,6 +28,7 @@ namespace AK
         static const AkUniqueID AMBIENT_NOISE = 564811028U;
         static const AkUniqueID CAR_SOUNDS = 2477948628U;
         static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID MAIN = 3161908922U;
         static const AkUniqueID ON_BUTTON_PRESS = 3919005661U;
         static const AkUniqueID PICKUP_SOUNDS = 1595075340U;
     } // namespace BANKS
