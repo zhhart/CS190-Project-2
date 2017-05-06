6676ce7d03e0c9d423eb45ea981089919eed3856
Steven Kuo <sykuo@uci.edu>
2017-05-05 18:56:00 -0700

    When Score Increments, play Eating sound
    + score --> player health, does this count as a non-linear status?

--- CS190 Project 2/Assets/Scripts/GameManager.cs
+++ CS190 Project 2/Assets/Scripts/GameManager.cs
@@ -7,7 +7,10 @@ public class GameManager : MonoBehaviour {
     /// <summary>
     /// An integer that represents the score
     /// </summary>
-    private int score = 0;
+	/// 
+	public GameObject player;
+
+	private int score = 0;
     public int Score
     {
         get { return score;  }
@@ -17,6 +20,7 @@ public class GameManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         score = 0;
+		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 	
 	// Update is called once per frame
@@ -30,6 +34,8 @@ public class GameManager : MonoBehaviour {
     public void incScore()
     {
         Score = Score + 1;
-        Debug.Log("Player Score: " + Score);
+        Debug.Log("Player Health: " + Score);
+		AkSoundEngine.PostEvent ("Pickup", player);
+
     }
 }
